a3c0cd5054edd0b90e63faee44e9b28e32d3fb32
tariq1010 <112605197+tariq1010@users.noreply.github.com>
2023-09-28 16:24:00 +0500

    updated

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  console.log("Just got a request!");
+  res.json({ message: "Just got a request! backend folder" });
+});
+app.listen(3002, () => {
+  console.log("running");
+});
