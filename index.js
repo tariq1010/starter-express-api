d5fb9db59598bcfa6687df94ea2f04f446dd1619
tariq1010 <112605197+tariq1010@users.noreply.github.com>
2023-09-28 16:19:22 +0500

    updated

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  console.log("Just got a request!");
+  res.json({ message: "Just got a request!" });
+});
+app.listen(3002, () => {
+  console.log("running");
+});
